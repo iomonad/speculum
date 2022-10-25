78466169177c3a2fad614a6af4739fded8638e35
iomonad <iomonad@riseup.net>
2022-10-25 23:05:15 +0200

    initial commit

---
+++ src/speculum/core.clj
@@ -0,0 +1,6 @@
+(ns speculum.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/speculum/core_test.clj
@@ -0,0 +1,7 @@
+(ns speculum.core-test
+  (:require [clojure.test :refer :all]
+            [speculum.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
