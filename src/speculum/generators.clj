58424eaf3ec0c3dcf8004ce203cbdf70bb14e653
iomonad <iomonad@riseup.net>
2022-12-02 00:21:04 +0100

    wip: generators

--- src/speculum/generators.clj
+++ src/speculum/generators.clj
@@ -1,1 +1,8 @@
-(ns speculum.generators)
+(ns speculum.generators
+  (:require [clojure.core.matrix :as m]))
+
+(defn generate-range
+  [level [ox oy] [tx ty]]
+  {:level level
+   :range nil; to be done
+   })
