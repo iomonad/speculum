fe913202a65f27009c3b5ebd118b645ac93c8f55
iomonad <iomonad@riseup.net>
2022-12-01 23:45:59 +0100

    dependencies: bump

--- project.clj
+++ project.clj
@@ -3,15 +3,15 @@
   :url "https://github.com/iomonad/speculum"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[buddy/buddy-auth             "2.1.0" :exclusions [cheshire]]
+  :dependencies [[buddy/buddy-auth             "3.0.323" :exclusions [cheshire]]
                  [buddy/buddy-core             "1.10.413"]
                  [cc.qbits/knit                "1.0.0"]
                  [clj-commons/fs               "1.6.310"]
                  [clj-http                     "3.12.3"]
                  [com.grammarly/omniconf       "0.4.3"]
                  [integrant                    "0.8.0"]
-                 [io.pedestal/pedestal.jetty   "0.5.5"]
-                 [io.pedestal/pedestal.service "0.5.5"]
+                 [io.pedestal/pedestal.jetty   "0.5.10"]
+                 [io.pedestal/pedestal.service "0.5.10"]
                  [medley                       "1.4.0"]
                  [metosin/muuntaja             "0.6.8"]
                  [metosin/reitit               "0.5.18"]
@@ -21,7 +21,7 @@
                  [org.clojure/core.match       "1.0.0"]
                  [org.clojure/tools.logging    "1.2.4"]
                  [prismatic/schema             "1.4.1"]
-                 [spootnik/unilog              "0.7.24"]]
+                 [spootnik/unilog              "0.7.31"]]
   :profiles {:dev {:dependencies [[integrant/repl               "0.3.2"]
                                   [org.clojure/tools.namespace  "1.3.0"]]
                    :source-paths ["dev"]
